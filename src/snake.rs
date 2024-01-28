327b266937dfca52bf53bfd3f20fd08df4af6527
Lincoln Ruteski <lruteski@gmail.com>
2024-01-28 14:20:39 -0300

    funcao de overloap_tail

--- src/snake.rs
+++ src/snake.rs
@@ -147,7 +147,25 @@ impl Snake {
    }
 
    pub fn restore_tail(&mut self) {
-      let block = self.tail.clone().unwrap();
-      self.body.push_back(block);
+      let blk = self.tail.clone().unwrap();
+      self.body.push_back(blk);
+   }
+
+   pub fn onverlap_tail(&self, x: i32, y: i32) -> bool {
+      let mut ch = 0;
+
+      for block in &self.body {
+         if x == block.x && y == block.y {
+            return true;
+         }
+
+         ch += 1;
+
+         if ch == self.body.len() -1 {
+            break;
+         }
+      }
+
+      return false;
    }
 }
