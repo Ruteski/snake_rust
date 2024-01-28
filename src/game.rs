7645fa56165759294c141780011e7229dc5bf871
Lincoln Ruteski <lruteski@gmail.com>
2024-01-28 14:26:59 -0300

    criacao do arquivo game

---
+++ src/game.rs
@@ -0,0 +1,18 @@
+use piston_window::*;
+use piston_window::types::Color;
+
+use rand::{thread_rng, Rng};
+
+use crate::snake::{Direction, Snake};
+use crate::draw::{draw_rectangle, draw_block};
+
+const FOOD_COLOR: Color = [0.80, 0.00, 0.00, 1.0];
+const BORDER_COLOR: Color = [0.00, 0.00, 0.00, 1.0];
+const GAMEOVER_COLOR: Color = [0.90, 0.00, 0.00, 0.5];
+
+const MOVING_PERIOD: f64 = 0.1;
+const RESTART_TIME: f64 = 1.0;
+
+pub struct Game {
+   
+}
